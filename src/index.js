7c8db9f8a2c9b3ac9d79e0a8b1f3af0370714740
Jan <40180321+britsja@users.noreply.github.com>
2023-08-07 14:28:27 +0000

    Updated to React 18 and testing images

--- src/index.js
+++ src/index.js
@@ -1,4 +1,49 @@
-var React = require("react");
-var ReactDom = require("react-dom");
+import React from "react";
+import { createRoot } from "react-dom/client";
 
-ReactDom.render(<h1>Hello World</h1>, document.getElementById("root"));
+const fname = "Jan";
+const lname = "Anon";
+
+const bonusFood = "Pizza";
+
+const image = "https://picsum.photos/400/400";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(
+  <div>
+    <h1 className="headerclass">
+      {fname} {lname} - Favorite Food Items
+    </h1>
+    <ul className="foodlist">
+      <li>Bacon</li>
+      <li>Cheese</li>
+      <li>Olives</li>
+      <li>{bonusFood}</li>
+    </ul>
+    <div className="imagecontainer">
+      <img
+        className="food-image"
+        alt="bacon image"
+        src="https://www.allrecipes.com/thmb/ZyYXwx4FKm3Y13OJGpUjVwx8Ftg=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/3882376-bacon-for-the-family-or-a-crowd-sanzoe-4x3-1-8711ddbb6af242f184a79e5e72efc7e3.jpg"
+      />
+      <img
+        className="food-image"
+        alt="cheese image"
+        src="https://static.onecms.io/wp-content/uploads/sites/43/2022/11/07/254478-homemade-mozzarella-cheese-ddmfs-4x3-1.jpg"
+      />
+      <img
+        className="food-image"
+        alt="olives image"
+        src="https://www.finedininglovers.com/sites/g/files/xknfdk626/files/styles/im_landscape_100/public/2023-02/olive-varieties%C2%A9iStock.jpg.webp?itok=Cusclg5o"
+      />
+      <img
+        className="food-image"
+        alt="pizza image"
+        src="https://hips.hearstapps.com/hmg-prod/images/classic-cheese-pizza-recipe-2-64429a0cb408b.jpg"
+      />
+    </div>
+    <p>Lucky number today is {Math.floor(Math.random() * 10)} !</p>
+    <img className="random-image" src={image} alt="random image" />
+  </div>
+);
